f5ef067aff510e6f5f7800f9f444de27ea726625
Christopher Bradley <cb8590@gadi-login-03.gadi.nci.org.au>
2024-05-13 15:17:25 +1000

    Spring valley dimensions and rotation/flippage correct
    Now to trial on the other sites and reimplement merging multiple orderids (without going over memory allocation)

--- Code/run_planet_xarray_3b.sh
+++ Code/run_planet_xarray_3b.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #PBS -N PlanetXarray
 #PBS -P xe2
+#PBS -q express
 #PBS -l mem=32GB
 #PBS -l jobfs=3GB
 #PBS -l ncpus=1
@@ -15,8 +16,7 @@ cd $wd
 source /g/data/xe2/John/geospatenv/bin/activate
 
 # Run python script 
-# python3 Code/planet_xarray_3b.py --indir /home/147/cb8590/practice/MULL --orderids 79f404e3-6b72-43fa-ac13-1b33d0afa755 --outpath /home/147/cb8590/practice/MULL_xarray_3b.pkl
-python3 Code/planet_xarray_3b.py --indir /g/data/xe2/datasets/Planet/Farms/ARBO --orderids 74dbb359-bf0e-4445-a643-7423ef87edf7 --outpath /home/147/cb8590/practice/ARBO_xarray_3b_1GB.pkl
+python3 Code/planet_xarray_3b.py --indir /g/data/xe2/datasets/Planet/Farms/SPRV --orderid 5fb7ea5a-05ec-4784-a2c1-e4a59540f914 --outpath /home/147/cb8590/practice/SPRV_xarray_3b_400MB.pkl
 
 # To submit
-# qsub repos/PaddockTS/Code/run_planet_xarray_3b.sh 
+# qsub repos/PaddockTS/Code/run_planet_xarray_3b.sh
