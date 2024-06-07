fa3ef1451a0616eab53cc57ee3a1a099ce84d1df
johnburley3000 <john.burley3000@gmail.com>
2024-06-07 11:06:11 +1000

    Update planet_xarray_8b.py
    Started with Chris' 3band version. Now should read in 8band data and also mask unusable pixels with udm2 in load_images()

--- Code/planet_xarray_8b.py
+++ Code/planet_xarray_8b.py
@@ -9,10 +9,10 @@ import argparse
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
-        description="""Load 3 band Planetscope data into an xarray named {stub}_xarray.pkl
+        description="""Load 8 band Planetscope data into an xarray named {stub}_xarray.pkl
         
 Example usage:
-python3 Code/01_pre-segment.py --indir /g/data/xe2/datasets/Planet/Farms/MULL/ --orderid 79f404e3-6b72-43fa-ac13-1b33d0afa755 --outpath /g/data/xe2/chris/Data/MULL_xarray_3b.pkl
+python3 Code/___.py --indir /g/data/xe2/datasets/Planet/Farms/MULL/ --orderid 79f404e3-6b72-43fa-ac13-1b33d0afa755 --outpath /g/data/xe2/chris/Data/MULL_xarray_8b.pkl
 """,
         formatter_class=argparse.RawTextHelpFormatter
     )
@@ -37,22 +37,34 @@ def find_timestamps(directory):
     timestamps = sorted(timestamps)
     return timestamps
 
-def load_images(directory, timestamps, tiff_prefix="_3B_Visual_clip"):
-    """Load all images without any preprocessing
+def load_images(directory, timestamps, tiff_prefix='_3B_AnalyticMS_SR_8b_clip' , udm_suffix="_3B_udm2_clip.tif"):
+    """Load images and mask with udm-2 using rasterio
+
+    Info on PlanetScope bands: https://developers.planet.com/docs/apis/data/sensors/
+    Info on udm2 bands: https://developers.planet.com/docs/data/udm-2/
     
     Returns
     -------
-    images:list of 3d-array
-        list of 3 dimensional arrays (band, x, y)
-        There are 4 bands (red, green, blue, alpha)
+    images: list of 3d-array
+        list of masked 8 dimensional arrays (band, x, y)
     """
     images = []
     for timestamp in timestamps:
         filename = directory + timestamp + tiff_prefix + ".tif"
-        src = rasterio.open(filename)
-        image = src.read()
-        images.append(image)
+        with rasterio.open(filename) as src:
+            image = src.read()  # Reads all bands; shape (band_count, height, width)
+
+        filemask = directory + timestamp + udm_suffix
+        with rasterio.open(filemask) as src_mask:
+            udm2 = src_mask.read(1)  # Read the first band for the mask "Regions of a scene that are free of cloud, haze, cloud shadow and/or snow"
+
+        clear_pixels = udm2.astype('float')
+        clear_pixels[clear_pixels == 0] = np.nan
+        masked_image = image * clear_pixels
+
+        images.append(masked_image)
     return images
+
 
 def create_datetimes(timestamps):
     """
@@ -77,7 +89,7 @@ def create_datetimes(timestamps):
         datetimestamps.append(datetimestamp)
     return datetimestamps
 
-def find_bbox(directory, timestamp, tiff_prefix='_3B_Visual_clip'):
+def find_bbox(directory, timestamp, tiff_prefix='_3B_AnalyticMS_SR_8b_clip'):
     """ Finds the bounding box from the metadata file"""
     filename = directory + timestamp + ".json"
     file = open(filename)
@@ -104,8 +116,8 @@ def create_lat_lon(bbox, shape):
     x = np.arange(bbox[1], bbox[3], pixel_size[1])
     return x, y
 
-def load_single_order_3band(base_directory, order_id):
-    """Loads all the images from a 3band planetscope folder into an xarray"""
+def load_single_order_8band(base_directory, order_id):
+    """Loads all the images from a 8band planetscope folder into an xarray"""
     directory = base_directory + "/" + order_id + "/PSScene/"
     timestamps = find_timestamps(directory)
     print(f"Number of timestamps in order {order_id}: {len(timestamps)}")
@@ -120,9 +132,14 @@ def load_single_order_3band(base_directory, order_id):
     y, x = create_lat_lon(bboxs[0], (transposed_images.shape[3], transposed_images.shape[2]))
     ds_planetscope = xr.Dataset(
         {
-            "nbart_red":(["time", "y", "x"], transposed_images[0]),
-            "nbart_green":(["time", "y", "x"], transposed_images[1]),
-            "nbart_blue":(["time", "y", "x"], transposed_images[2]),
+        "Coastal Blue":(["time", "y", "x"], flipped_transposed_image[0]),
+        "nbart_blue":(["time", "y", "x"], flipped_transposed_image[1]),
+        "Green I":(["time", "y", "x"], flipped_transposed_image[2]),
+        "nbart_green":(["time", "y", "x"], flipped_transposed_image[3]),
+        "Yellow":(["time", "y", "x"], flipped_transposed_image[4]),
+        "nbart_red":(["time", "y", "x"], flipped_transposed_image[5]),
+        "Red Edge":(["time", "y", "x"], flipped_transposed_image[6]),
+        "nbart_nir_1":(["time", "y", "x"], flipped_transposed_image[7]),
         }, coords={
             "time": datetimestamps,
             "y": ("y", y),
@@ -138,11 +155,11 @@ if __name__ == '__main__':
     order_id = args.orderid
     outpath = args.outpath
 
-    print(f"{datetime.now()} Starting planet_xarray_3b in {base_directory} for {order_id}")
+    print(f"{datetime.now()} Starting planet_xarray_8b in {base_directory} for {order_id}")
 
-    xarray = load_single_order_3band(base_directory, order_id)
+    xarray = load_single_order_8band(base_directory, order_id)
 
     with open(outpath, 'wb') as handle:
         pickle.dump(xarray, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    print(f"{datetime.now()} Finished planet_xarray_3b and exported to {outpath}")
+    print(f"{datetime.now()} Finished planet_xarray_8b and exported to {outpath}")
