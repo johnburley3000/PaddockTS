bedc30f05d7cc0e641f8452bedd2951c3c3a20a1
Christopher Bradley <cb8590@gadi-login-09.gadi.nci.org.au>
2024-06-13 22:18:24 +1000

    Full pipeline for SILO climate videos
    Scale is dodgy atm though

--- Notebooks/plotly_example_animation.py
+++ Notebooks/plotly_example_animation.py
@@ -4,40 +4,71 @@ from matplotlib import animation
 from datetime import datetime
 
 # +
-# Load the xarray
-silo_filepath = "/g/data/ub8/au/SILO/daily_rain/2023.daily_rain.nc"
-silo_full = xr.open_dataset(silo_filepath)
-
 # Region of interest
 lat = -34.38904277303204
 lon = 148.46949938279096
-buffer = 1
+buffer = 1  # 1 degree ~= 100km
 
-silo_region = silo_full.sel(lat=slice(lat - buffer, lat + buffer), lon=slice(lon - buffer, lon + buffer))
-ds = silo_region.sel(time=slice('2023-01-01', '2023-12-01'))
+# Load the silo data for 2017-2023 for just the region of interest
+# variables = ["daily_rain", "max_temp", "min_temp", "radiation", "vp"]
+variables = ["radiation", "vp"]
 
-rainfall = ds['daily_rain'] 
-times = ds['time']
-
-# +
-# Create the animation
-fig, ax = plt.subplots(figsize=(10, 6))
-def animate(time_index):
-    ax.clear()
-    time_slice = rainfall.isel(time=time_index)
-    c = ax.pcolormesh(time_slice['lon'], time_slice['lat'], time_slice, shading='auto', cmap='Blues')
-    if not hasattr(ax, 'colorbar'):
-        ax.colorbar = fig.colorbar(c, ax=ax, label='Rainfall')
-    ax.set_title(f"Rainfall Data on {str(times[time_index].values)[:10]}")
-    ax.set_xlabel('Longitude')
-    ax.set_ylabel('Latitude')
+print("Loading SILO variables: ", variables)
 
 start = datetime.now()
-ani = animation.FuncAnimation(fig, animate, frames=len(times), interval=33)
-ani.save('rainfall_animation.mp4', writer='ffmpeg')
+silo_data = {}
+for variable in variables:
+    silo_filepaths = [
+        f"/g/data/ub8/au/SILO/{variable}/2017.{variable}.nc",
+        f"/g/data/ub8/au/SILO/{variable}/2018.{variable}.nc",
+        f"/g/data/ub8/au/SILO/{variable}/2019.{variable}.nc",
+        f"/g/data/ub8/au/SILO/{variable}/2020.{variable}.nc",
+        f"/g/data/ub8/au/SILO/{variable}/2021.{variable}.nc",
+        f"/g/data/ub8/au/SILO/{variable}/2022.{variable}.nc",
+        f"/g/data/ub8/au/SILO/{variable}/2023.{variable}.nc"
+    ]
+    
+    # Load the data for each year and select just the region of interest 
+    silo_datasets = []
+    for filepath in silo_filepaths:
+        ds = xr.open_dataset(filepath)
+        ds_region = ds.sel(lat=slice(lat - buffer, lat + buffer), lon=slice(lon - buffer, lon + buffer))
+        silo_datasets.append(ds_region)
+    combined_ds = xr.concat(silo_datasets, dim='time')
+    silo_data[variable] = combined_ds[variable]
+    
 end = datetime.now()
+print("Time taken to load SILO datasets: ", end - start)
 
-print(end - start)
+# +
+# variables = ["daily_rain", "max_temp", "min_temp", "radiation", "vp"]
+# colours = ["Blues", "Reds", "Greens", "Oranges", "Purples"] 
+variables = ["radiation", "vp"]
+colours = ["Oranges", "Purples"] 
+
+for variable, colour in zip(variables, colours):
+    ds = silo_data[variable]
+    time_series = ds 
+    times = ds['time']
+
+    # Create the animation
+    fig, ax = plt.subplots(figsize=(10, 6))
+    def animate(time_index):
+        ax.clear()
+        time_slice = time_series.isel(time=time_index)
+        c = ax.pcolormesh(time_slice['lon'], time_slice['lat'], time_slice, shading='auto', cmap=colour)
+        if not hasattr(ax, 'colorbar'):
+            ax.colorbar = fig.colorbar(c, ax=ax, label=variable)
+        ax.set_title(f"{variable} on {str(times[time_index].values)[:10]}")
+        ax.set_xlabel('Longitude')
+        ax.set_ylabel('Latitude')
+
+    start = datetime.now()
+    ani = animation.FuncAnimation(fig, animate, frames=len(times), interval=33)
+    ani.save(f'{variable}_animation.mp4', writer='ffmpeg')
+    end = datetime.now()
+
+    print(f"Time taken to create animation for {variable} {end - start}")
 # -
 
 
