1b751b1df53b0946143d94af255c8b6d564fb63b
Christopher Bradley <cb8590@gadi-login-09.gadi.nci.org.au>
2024-08-08 15:48:09 +1000

    Created anuclimate_daily.csv

--- Notebooks/DAESIM_preprocess/ozwald_8day.py
+++ Notebooks/DAESIM_preprocess/ozwald_8day.py
@@ -5,17 +5,50 @@ import pandas as pd
 import glob
 
 
+# +
+# var="Ssoil"
+# latitude=-34.3890427
+# longitude=148.469499
+# year="2021"
+# buffer = 0.0000000001    # This buffer is less than the grid size of 500m (0.005 degrees), so you get a single point
+
+# north = lat + buffer 
+# south = lat - buffer 
+# west = lon - buffer
+# east = lon + buffer
+
+# time_start = f"{year}-01-01"
+# time_end = f"{year}-03-31"
+
+# # base_url = "https://thredds.nci.org.au"  # This is the new url (dapds00 is supposedly deprecated), but LAI only works with the old url
+# base_url = "https://dapds00.nci.org.au"
+# url = f'{base_url}/thredds/ncss/grid/ub8/au/OzWALD/8day/{var}/OzWALD.{var}.{year}.nc?var={var}&north={north}&west={west}&east={east}&south={south}&time_start={time_start}&time_end={time_end}' 
+# print(url)
+
+# response = requests.get(url)
+# filename = f'{var}_{year}.nc' 
+# with open(filename, 'wb') as f:
+#     f.write(response.content)
+
+# ds = xr.open_dataset(filename)
+# df = ds.to_dataframe().reset_index()
+# df_indexed = df.set_index('time')
+# df_dropped = df_indexed.drop(columns=['latitude', 'longitude'])
+
+# print(west, north, east, south)
+# -
+
 def ozwald_singleyear(var="Ssoil", latitude=-34.3890427, longitude=148.469499, year="2021"):
     
     buffer = 0.0000000001    # This buffer is less than the grid size of 500m (0.005 degrees), so you get a single point
     
-    north = lat + buffer 
-    south = lat - buffer 
-    west = lon - buffer
-    east = lon + buffer
+    north = latitude + buffer 
+    south = latitude - buffer 
+    west = longitude - buffer
+    east = longitude + buffer
     
     time_start = f"{year}-01-01"
-    time_end = f"{year}-03-31"
+    time_end = f"{year}-12-31"
     
     # base_url = "https://thredds.nci.org.au"  # This is the new url (dapds00 is supposedly deprecated), but LAI only works with the old url
     base_url = "https://dapds00.nci.org.au"
@@ -64,7 +97,6 @@ df = ozwald_multivariable()
 df.head()
 
 abbreviations = {
-    "time":"date",
     "Ssoil":"Soil moisture",
     "Qtot":"Runoff",
     "LAI":"Vegetation leaf area",
@@ -74,4 +106,4 @@ df.rename(columns=abbreviations, inplace=True)
 df.rename_axis("date", inplace=True)
 df.head()
 
-df.to_csv("DAESim_forcing.csv")
+df.to_csv("ozwald_8day.csv")
