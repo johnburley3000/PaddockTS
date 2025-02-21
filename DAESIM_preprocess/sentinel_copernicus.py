95558120be5fb5b32a063c8904bbc356dd45b2e1
Christopher Bradley <chris2bradley@gmail.com>
2025-02-21 17:37:02 +1100

    Cleaned up multi-timepoint download a bit

--- DAESIM_preprocess/sentinel_copernicus.py
+++ DAESIM_preprocess/sentinel_copernicus.py
@@ -1,4 +1,5 @@
-# !pip install sentinelhub
+# +
+# # !pip install sentinelhub
 
 # +
 # Need to create a Copernicus CLIENT_ID and CLIENT_SECRET by following these instructions
@@ -11,7 +12,7 @@
 # Useful links:
 # Example notebook demo-ing Sentinel Hub: https://github.com/eu-cdse/notebook-samples/blob/c0e0ade601973c5d4e4bf66a13c0b76ebb099805/sentinelhub/migration_from_scihub_guide.ipynb
 # Copernicus API reference is here: https://documentation.dataspace.copernicus.eu/APIs/SentinelHub/ApiReference.html
-# SentinelHub ReadTheDocs is here: https://sentinelhub-py.readthedocs.io/en/latest/examples/process_request.html
+# SentinelHub ReadTheDocs is here: https://sentinelhub-py.readthedocs.io/en/latest/examples/process_request.html#Example-8-:-Multiple-timestamps-data
 # SentinelHub Usage allowance is here: https://shapps.dataspace.copernicus.eu/dashboard/#/
 # Note: Free allowance is 30,000 units = (5km x 5km x 3 bands) x 300 timepoints x 100 locations
 
@@ -40,12 +41,8 @@ from sentinelhub import (
     bbox_to_dimensions, SHConfig, SentinelHubCatalog, Geometry
 )
 
-# For using their plotting function
-from typing import Any, Optional, Tuple
-import matplotlib.pyplot as plt
-
 # +
-# Load your Copernicus Client ID and Secret from a json file that doesn't get committed to the repository
+# Load your Copernicus Client ID and Secret from a json file (or copy here, but don't commit)
 with open("credentials.json", "r") as file:
     credentials = json.load(file)
     
@@ -54,72 +51,44 @@ CLIENT_SECRET = credentials["CLIENT_SECRET"]
 
 print("Client ID:", CLIENT_ID)
 print("Client Secret:", CLIENT_SECRET) 
-
-TOKEN_URL = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
 # -
 
+# Setup the authentication
 config = SHConfig()
 config.sh_client_id = CLIENT_ID
 config.sh_client_secret = CLIENT_SECRET
 config.sh_token_url = "https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token"
 config.sh_base_url = "https://sh.dataspace.copernicus.eu"
 
+# Define the region of interest and date
+aoi_coords_wgs84 = [148.46449900000002, -34.394042000000006, 148.474499, -34.384042]
+time_interval = ('2022-07-01', '2022-10-20')
+max_cloud_percent = 10
+resolution = 10
+# Calculate the bounding box and resolution
+aoi_bbox = BBox(bbox=aoi_coords_wgs84, crs=CRS.WGS84)
+aoi_size = bbox_to_dimensions(aoi_bbox, resolution=resolution)
+print(f'Image shape at {resolution} m resolution: {aoi_size} pixels')
+
+# Search the catalogue for images at this location, timerange and cloud percent 
+catalog = SentinelHubCatalog(config=config)
 data_collection = DataCollection.SENTINEL2_L2A.define_from(
                 name="s2", service_url="https://sh.dataspace.copernicus.eu"
             )
-
-
-# +
-def define_query(lat=-34.389042, lon=148.469499, buffer=0.005 , start_year=2020, end_year=2021):
-    """Just requesting the bands needed for RGB and fractional cover"""
-    lat_range = (lat-buffer, lat+buffer)
-    lon_range = (lon-buffer, lon+buffer)
-    time_range = (f'{start_year}-01-01', f'{end_year}-12-31')
-    query = {
-        'y': lat_range,
-        'x': lon_range,
-        'time': time_range,
-        'resolution': (-10, 10),
-        'measurements': ['nbart_red', 'nbart_green', 'nbart_blue', 'nbart_nir_1', 'nbart_swir_2','nbart_swir_3'],
-
-    }
-    return query
-    
-lat = -34.389042
-lon = 148.469499
-buffer = 0.005    # 0.01 is 1km in each direction to 2kmx2km total     
-start_year = 2010  # This automatically gets the earlist timepoint (late 2015)
-end_year = 2030    # This automatically gets the most recent timepoint
-
-query = define_query(lat, lon, buffer, start_year, end_year)
-query
-
-# +
-# aoi_coords_wgs84 = [4.20762, 50.764694, 4.487708, 50.916455]
-aoi_coords_wgs84 = [148.46449900000002, -34.394042000000006, 148.474499, -34.384042]
-
-time_interval = ('2022-07-01', '2022-10-20')
-
-resolution = 10
-aoi_bbox = BBox(bbox=aoi_coords_wgs84, crs=CRS.WGS84)
-aoi_size = bbox_to_dimensions(aoi_bbox, resolution=resolution)
-
-print(f'Image shape at {resolution} m resolution: {aoi_size} pixels')
-# -
-catalog = SentinelHubCatalog(config=config)
 search_iterator = catalog.search(
     data_collection,
     bbox=aoi_bbox,
     time=time_interval,
-    filter="eo:cloud_cover < 10",
+    filter=f"eo:cloud_cover < {max_cloud_percent}",
     fields={"include": ["id", "properties.datetime"], "exclude": []},
 )
-
 results = list(search_iterator)
-results
-
 unique_dates = set(result['properties']['datetime'][:10] for result in results)
 unique_dates
+
+# +
+# Setup a minimal evaluation script.
+# Can add to this script things like masking cloud cover and calculating NDVI
 
 evalscript_true_color = """
     //VERSION=3
@@ -140,42 +109,36 @@ evalscript_true_color = """
     }
 """
 
-
-# +
-# Multitimestamp data
-# -
-
 def get_true_color_request(date, bbox, size):
     return SentinelHubRequest(
         evalscript=evalscript_true_color,
         input_data=[
             SentinelHubRequest.input_data(
-                data_collection=DataCollection.SENTINEL2_L2A.define_from(
-                name="s2", service_url="https://sh.dataspace.copernicus.eu"
-            ),
+                data_collection=data_collection,
                 time_interval=(date, date),
             )
         ],
-        responses=[SentinelHubRequest.output_response("default", MimeType.PNG)],
+        responses=[SentinelHubRequest.output_response("default", MimeType.TIFF)],
         bbox=bbox,
         size=size,
         config=config,
     )
 
-
-# +
-# create a list of requests
-list_of_requests = [get_true_color_request(date) for date in unique_dates]
+list_of_requests = [get_true_color_request(date, aoi_bbox, aoi_size) for date in unique_dates]
 list_of_requests = [request.download_list[0] for request in list_of_requests]
+# -
 
-# download data with multiple threads
+# Download the bands
 data = SentinelHubDownloadClient(config=config).download(list_of_requests, max_threads=5)
+len(data)
+
+data[0].shape
 
 # +
-# some stuff for pretty plots
+# Visualising the data
 ncols = 4
 nrows = 3
-aspect_ratio = betsiboka_size[0] / betsiboka_size[1]
+aspect_ratio = aoi_size[0] / aoi_size[1]
 subplot_kw = {"xticks": [], "yticks": [], "frame_on": False}
 
 fig, axs = plt.subplots(ncols=ncols, nrows=nrows, figsize=(5 * ncols * aspect_ratio, 5 * nrows), subplot_kw=subplot_kw)
@@ -183,6 +146,19 @@ fig, axs = plt.subplots(ncols=ncols, nrows=nrows, figsize=(5 * ncols * aspect_ra
 for idx, image in enumerate(data):
     ax = axs[idx // ncols][idx % ncols]
     ax.imshow(np.clip(image * 2.5 / 255, 0, 1))
-    ax.set_title(f"{slots[idx][0]}  -  {slots[idx][1]}", fontsize=10)
+    ax.set_title(f"{list(unique_dates)[idx]}", fontsize=10)
 
 plt.tight_layout()
+
+# +
+# TODO
+# 
+# Allow user to add more bands like in this example: https://sentinelhub-py.readthedocs.io/en/latest/examples/process_request.html#Example-3:-All-Sentinel-2's-raw-band-values
+# Mask out clouds like in this example: https://docs.sentinel-hub.com/api/latest/data/sentinel-2-l2a/examples/#true-color-cloudy-pixels-masked-out
+# Combine into xarray with coordinates (not sure if we can download images with metadata directly or not)
+# Compare with imagery from DEA
+# -
+
+
+
+
